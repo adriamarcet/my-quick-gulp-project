af9631bc43f0ec0b308985ad702bc32284374631
Adrià Marcet <adriamarcetrovira@gmail.com>
2017-09-15 02:14:35 +0200

    added optim tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 /* 	============================================
-	Require
+	Requires
 ================================================ */
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp 			= require('gulp');
+const sass 			= require('gulp-sass');
+const browserSync 	= require('browser-sync').create();
+const useref 		= require('gulp-useref');
+const uglify 		= require('gulp-uglify');
+const gulpIf 		= require('gulp-if');
+const cssnano 		= require('gulp-cssnano');
+const imagemin 		= require('gulp-imagemin');
+const cache 		= require('gulp-cache');
+
+
+
 
 
 /* 	============================================
@@ -39,3 +48,36 @@ gulp.task('browserSync', function () {
 	})
 });
 
+// Useref
+	// it builds in one file different from many locations
+	// it will look for the html tag <!-- build --> and <!-- endbuild -->
+gulp.task('useref', function () {
+	return gulp.src('app/*.html') // get source files
+		.pipe(useref() ) // send it to gulp plugin
+
+		// Minifies only if it's a JavaScript file
+		.pipe(gulpIf('*.js', uglify() ) ) 
+		
+		// Minifies only if it's a CSS file
+		.pipe(gulpIf('*.css', cssnano()))
+		// output folder
+		.pipe(gulp.dest('dist')) 
+});
+
+// Images
+gulp.task('images', function () {
+	return gulp.src('app/images/**/*.+(jpg|png|svg|gif)') // get source files
+		.pipe(
+
+			cache( 
+
+				imagemin({
+					interlaced: true,
+					progressive: true,
+					optimizationLevel: 5,
+					svgoPlugins: [{removeViewBox: true}]
+				}) // optimize images
+			) // cache the task
+		) // send it to gulp plugin
+		.pipe(gulp.dest('dist/images')) // output folder
+});
